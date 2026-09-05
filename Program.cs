c89f75e1045e716856e71af64e859c801cb56b1d
Mat <matze.krieger@googlemail.com>
2026-09-05 13:02:39 +0200

    added new lines

--- Program.cs
+++ Program.cs
@@ -12,13 +12,17 @@ class SampleClass
         Console.WriteLine("\nDate/Time Manipulation:");
         HClass.HumanizeDates();
 
-        Console.WriteLine("\n");
+        Console.WriteLine("\n\n");
 
         var fib = FClass.Fibonacci(10);
         Console.WriteLine(string.Join(", ", fib));
+        
+        Console.WriteLine("\n\n");
 
         Trace.WriteLine("This is a trace message when tracing the app.");
         Debug.WriteLine("This is a debug message just for developers.");
+        
+        Console.WriteLine("\n\n");
     }
 
 
