f76ae9db2a9f8c14f6918f7dfbd184f425a3c95c
Mat <matze.krieger@googlemail.com>
2026-09-05 13:07:56 +0200

    added debug messages

--- methods/FibonacciMethod.cs
+++ methods/FibonacciMethod.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Diagnostics;
 
 namespace FibonacciMethod
 {
@@ -6,6 +7,9 @@ namespace FibonacciMethod
     {
         public static int[] Fibonacci(int n)
         {
+            Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+            Debug.WriteLine($"We are looking for {n} numbers");
+
             int n1 = 0;
             int n2 = 1;
 
@@ -21,7 +25,12 @@ namespace FibonacciMethod
 
                 n1 = n2;
                 n2 = next;
+
+                Debug.WriteLineIf(next == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");
             }
+
+            // If n2 is 5 continue, else break.
+            Debug.Assert(n2 == 5, "The return value is not 5 and it should be.");
 
             return res.ToArray();
         }
