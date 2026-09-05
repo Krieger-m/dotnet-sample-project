8339158922d89dda84659b0e0761deaa98f78259
Mat <matze.krieger@googlemail.com>
2026-09-05 13:21:47 +0200

    commented out breaking debug assertion

--- methods/FibonacciMethod.cs
+++ methods/FibonacciMethod.cs
@@ -29,7 +29,7 @@ namespace FibonacciMethod
                 Debug.WriteLineIf(next == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");
             }
 
-            // If n2 is 5 continue, else break.
+              // If n2 is 5 continue, else break.
             // Debug.Assert(n2 == 5, "The return value is not 5 and it should be.");
 
             return res.ToArray();
